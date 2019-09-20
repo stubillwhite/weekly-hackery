e37bcdc0b8e43005966b531893c2eac838d134c1
Stu W <stubillwhite@gmail.com>
2019-09-20 21:58:37 +0100

    Add continue or exit loop

--- WeeklyHackery/src/roguelike/Roguelike.scala
+++ WeeklyHackery/src/roguelike/Roguelike.scala
@@ -3,6 +3,7 @@ package roguelike
 import roguelike.internal.creatures
 import roguelike.internal.creatures.{Combat, Goblin, Player, resolveCombat}
 
+import scala.io.StdIn
 import scala.util.Random
 
 object Roguelike {
@@ -13,12 +14,31 @@ object Roguelike {
     player.addItems("Gold", gold)
   }
 
-  def main(args: Array[String]): Unit = {
-    val combat = Combat(new Player(), new Goblin())
-    resolveCombat(combat)
-    lootCorpse(combat.player)
+  def gameLoop(player: Player): Unit = {
+    println("Combat ensues!")
 
-    println(combat.player)
+    val combat = Combat(player, new Goblin())
+    resolveCombat(combat)
+
+    if (combat.player.health == 0) {
+      println("You died!")
+    }
+    else {
+      lootCorpse(combat.player)
+
+      println()
+      println(s"Player health: ${player.healthStat()}, loot: ${player.inventory.getOrElse("Gold", 0)} gold")
+      println()
+      println("[C]ontinue or [E]xit")
+      StdIn.readChar() match {
+        case 'c' => gameLoop(player)
+        case 'e' => println(s"You escaped with ${combat.player.inventory.getOrElse("Gold", 0)} gold")
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    gameLoop(new Player())
     println("Done")
   }
 }
